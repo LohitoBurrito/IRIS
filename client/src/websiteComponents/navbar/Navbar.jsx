8b11d536b5f854279cd585d3ba56e6e9084d454b
lohitm2 <lohit308@gmail.com>
2023-05-12 17:00:09 -0500

    Logo Edit and Navbar Changes

--- client/src/websiteComponents/navbar/Navbar.jsx
+++ client/src/websiteComponents/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import { HashLink as Link } from 'react-router-hash-link'
 import { RiArrowRightCircleLine } from 'react-icons/ri'
+import { HiMenu } from 'react-icons/hi'
 import './navbar.css'
 
 const arrow = {
@@ -13,23 +15,77 @@ const arrow = {
 }
 
 function Navbar(props) {
-    return (
-        <div className='navbar'>
-            <div className='left'>
-                <Link className="logo" to="/main">IRIS</Link>
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    const [minimizeNav, setMinimizeNav] = useState(window.innerWidth < 1015 ? true : false);
+    const [openNavbar, setOpenNavbar] = useState("menubarClose"); 
+    const [menuFixed, setMenuFixed] = useState("menu");
+    const [navbar, setNavbar] = useState("navbar2");
+
+    useEffect(() => {
+        function handleResize() {
+          console.log(window.innerWidth)
+          setScreenWidth(window.innerWidth);
+          if (window.innerWidth < 1015) {
+            console.log("minimize nav")
+            setMinimizeNav(true)
+          } else {
+            console.log("dont minimize nav")
+            setMinimizeNav(false)
+          }
+        }
+        window.addEventListener('resize', handleResize);
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+
+    if (!minimizeNav) {
+        return (
+            <div className="navbar">
+                <Link className="left" to="/main"></Link>
+                <div className='right'>
+                    <Link className="navlink" to="/main">HOME</Link>
+                    <Link className="navlink" to="/main#about">ABOUT</Link>
+                    <Link className="navlink" to="/members">MEMBERS</Link>
+                    <Link className="navlink" to="/outreach">OUTREACH</Link>
+                    <Link className="navlink" to="/calendar">CALENDAR</Link>
+                    <Link className="navlink" to="/contact">CONTACT</Link>
+                    <Link className="navlink" to="/sponsor">SPONSORS</Link>   
+                    <Link className="navlink" id="fill" to="/join">JOIN <RiArrowRightCircleLine style={ arrow }/></Link> 
+                </div>                             
             </div>
-            <div className='right'>
-                <Link className="navlink" to="/main">HOME</Link>
-                <Link className="navlink" to="/main#about">ABOUT</Link>
-                <Link className="navlink" to="/members">MEMBERS</Link>
-                <Link className="navlink" to="/outreach">OUTREACH</Link>
-                <Link className="navlink" to="/calendar">CALENDAR</Link>
-                <Link className="navlink" to="/contact">CONTACT</Link>
-                <Link className="navlink" to="/sponsor">SPONSORS</Link>   
-                <Link className="navlink" id="fill" to="/join">JOIN <RiArrowRightCircleLine style={ arrow }/></Link> 
-            </div>                             
-        </div>
-    )
+        )
+    } else {
+        return (
+            <>
+                <div className={navbar}>
+                    <Link className="left" to="/main"></Link>
+                    <HiMenu className={menuFixed} onClick={() => {
+                        console.log("hey")
+                        if (openNavbar === "menubarOpen") {
+                            setOpenNavbar("menubarClose")
+                            setMenuFixed("menu")
+                            setNavbar("navbar2")
+                        } else {
+                            setOpenNavbar("menubarOpen")
+                            setMenuFixed("menuFixed")
+                            setNavbar("navbar3")
+                        }
+                    }}/>
+                </div>          
+                <div className={openNavbar}>
+                    <Link className="navlink" to="/main">HOME</Link>
+                    <Link className="navlink" onClick={() => setOpenNavbar("menubarClose")} to="/main#about">ABOUT</Link>
+                    <Link className="navlink" to="/members">MEMBERS</Link>
+                    <Link className="navlink" to="/outreach">OUTREACH</Link>
+                    <Link className="navlink" to="/calendar">CALENDAR</Link>
+                    <Link className="navlink" to="/contact">CONTACT</Link>
+                    <Link className="navlink" to="/sponsor">SPONSORS</Link>   
+                    <Link className="navlink" to="/join">JOIN</Link> 
+                </div>
+            </>
+        )
+    }
 }
 
 export default Navbar
